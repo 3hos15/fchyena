42bede1301309752f3712d7f1157abbe506eb801
3hos15 <145386421+3hos15@users.noreply.github.com>
2024-09-30 16:47:58 +0200

    30 sep

--- script/script.js
+++ script/script.js
@@ -1,4 +1,4 @@
-console.log("hi");
+console.log("Welkom!!!!!!!!!!!!!!!!!!!!!! :D");
 
 let theButton = document.querySelector("header button");
 let theNav = document.querySelector("nav");
@@ -10,32 +10,6 @@ function toggleMenu() {
   theButton.classList.toggle("toonMenu")
 }
 
-// bron: ChatGPT
-// promt: Could you write js code for this so that when you press on one of the stacked sections, it comes to the front. here is my code (html + css toegevoegd)
-// link naar gesprek https://chatgpt.com/share/66f27b39-9704-8002-8881-439e48489956
-
-// Select all the section elements inside the main container
-let sections = document.querySelectorAll('main > section > section');
-
-// Function to handle bringing the clicked section to the front
-// sections.forEach(section => {
-//     section.addEventListener('click', function() {
-//         // Remove the expanded class from all sections
-//         sections.forEach(s => s.classList.remove('expanded'));
-        
-//         // Increase z-index and add the expanded class to the clicked section
-//         this.classList.add('expanded');
-//         this.style.zIndex = 10;  // Bring to the front
-
-//         // Reset the z-index of all other sections
-//         sections.forEach(s => {
-//             if (s !== this) {
-//                 s.style.zIndex = '';  // Reset to default
-//             }
-//         });
-//     });
-// });
-
 const eersteSection = document.querySelector("section section:nth-of-type(1)");
 const tweedeSection = document.querySelector("section section:nth-of-type(2)");
 const derdeSection = document.querySelector("section section:nth-of-type(3)");
@@ -46,7 +20,7 @@ function makeSectionEersteActive() {
 	tweedeSection.classList.remove("active");
     derdeSection.classList.remove("active");
     vierdeSection.classList.remove("active");
-    console.log("Section 1 is actief");
+    console.log("Section Films is actief");
 }
 
 eersteSection.onclick = makeSectionEersteActive;
@@ -56,7 +30,7 @@ function makeSectionTweedeActive() {
 	eersteSection.classList.remove("active");
     derdeSection.classList.remove("active");
     vierdeSection.classList.remove("active");
-    console.log("Section 2 is actief");
+    console.log("Section Eten is actief");
 }
 
 tweedeSection.onclick = makeSectionTweedeActive;
@@ -66,7 +40,7 @@ function makeSectionDerdeActive() {
     eersteSection.classList.remove("active");
 	tweedeSection.classList.remove("active");
     vierdeSection.classList.remove("active");
-    console.log("Section 3 is actief");
+    console.log("Section Nieuws is actief");
 }
 
 derdeSection.onclick = makeSectionDerdeActive;
@@ -76,25 +50,8 @@ function makeSectionVierdeActive() {
     eersteSection.classList.remove("active");
 	tweedeSection.classList.remove("active");
     derdeSection.classList.remove("active"); 
-    console.log("Section 4 is actief");
+    console.log("Section Info is actief");
 }
 
 vierdeSection.onclick = makeSectionVierdeActive;
 
-// Select all the section elements inside the main container
-// let sections = document.querySelectorAll('main > section > section');
-
-// // Add a click event listener to each section
-// sections.forEach((section, index) => {
-//     section.addEventListener('click', function() {
-//         // Remove the active class from all sections
-//         sections.forEach(s => s.classList.remove('active'));
-
-//         // Add the active class to the clicked section
-//         section.classList.add('active');
-
-//         // Optionally, you can adjust z-index if you want to bring the clicked section to the front
-//         sections.forEach(s => s.style.zIndex = '');
-//         section.style.zIndex = '9999';  // Bring to the front
-//     });
-// });
